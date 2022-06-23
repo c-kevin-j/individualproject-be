ebba04aeac952861ce91e6f04ffe4b93f9b0fead
c-kevin-j <kevin_julius@srwasia.com>
2022-06-23 22:41:14 +0700

    adjust postrouter

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -3,15 +3,15 @@ const { readToken } = require("../config/encryption");
 const route = require("express").Router();
 
 route.get("/get", postsController.getAllPosts);
-route.get("/get/:id", postsController.getPosts);
 route.get("/get/detail", postsController.detailPost);
 route.get("/get/userPost", postsController.getUserPosts);
 route.get("/get/likedPost", postsController.getLikedPosts);
+route.get("/get/:id", postsController.getPosts);
 
 route.post("/add", readToken, postsController.addPost);
 route.patch("/edit", readToken, postsController.editPost);
 route.delete("/delete", readToken, postsController.deletePost);
-route.post("/comment", postsController.addComment);
+route.post("/comment", readToken, postsController.addComment);
 
 route.post("/like", readToken, postsController.addLike);
 route.delete("/unlike", readToken, postsController.removeLike);
